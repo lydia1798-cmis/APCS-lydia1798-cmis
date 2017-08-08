2d60dab74e7981acabf7615184a457fca86455ef
cmis <cmis@cmis-hs08.(none)>
2017-08-08 11:50:43 +0700

    Tacos

---
+++ PlayAround/HelloTacos.java
@@ -0,0 +1,10 @@
+public class HelloTacos
+    {
+        public static void main (String args [] )
+        {
+            System.out.println( "Hello all tacos out there! I hope that you and all of your other friends (burritos, enchilads, nachos, etc.) are having a good tim just living life. Please fall from the sky ASAP. Thanks, Lydia! ");
+            
+        } //end method main
+        
+    } // end method class HelloTacos
+    

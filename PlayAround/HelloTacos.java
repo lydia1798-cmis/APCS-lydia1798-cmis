ca6f6964ae71b3c47cc07a77af13dbaaad8832da
Lydia Ritchie <lydiaritchie@Lydias-MacBook-Air.local>
2017-08-08 22:01:54 +0700

    This is working on my personal computer

--- PlayAround/HelloTacos.java
+++ PlayAround/HelloTacos.java
@@ -2,7 +2,7 @@ public class HelloTacos
     {
         public static void main (String args [] )
         {
-            System.out.println( "Hello all tacos out there! I hope that you and all of your other friends (burritos, enchilads, nachos, etc.) are having a good tim just living life. Please fall from the sky ASAP. Thanks, Lydia! ");
+            System.out.println( "Hello all tacos out there! I hope that you and all of your other friends (burritos, enchilads, nachos, etc.) are having a good time just living life. Please fall from the sky ASAP. Thanks, Lydia! ");
             
         } //end method main
         

208fa003f360118fbf47625a1b2af68e1b08a73b
Lydia Ritchie <lydiaritchie@Lydias-MacBook-Air.local>
2017-08-11 22:52:13 +0700

    Finished the first part of the first problem

--- Fundamentals/Fundamentals.java
+++ Fundamentals/Fundamentals.java
@@ -2,12 +2,38 @@
 public class Fundamentals
 
 {
-    
+
     public static void main (String[] args){
         dataTypes();
-        }
-   public static void dataTypes(){
-       byte byteExample = 58;
-       System.out.println( byteExample );
+    }
+
+    public static void dataTypes(){
+        System.out.println( "dataTypes:\n" );
+
+        byte byteExample = 58;
+        System.out.println( "byte: " + byteExample + "\n");
+
+        short shortExample = -23678;
+        System.out.println( "short: " + shortExample + "\n");
+
+        int intExample = 45758678; 
+        System.out.println( "int: " + intExample + "\n");
+
+        long longExample = -2^50;
+        System.out.println("long: " + longExample + "\n");
+
+        float floatExample = 56.6f;
+        System.out.println("float: " + floatExample + "\n");
+
+        double doubleExample = 78889.87;
+        System.out.println("double: " + doubleExample + "\n");
+
+        boolean booleanExample = true;
+        System.out.println("boolean: " + booleanExample + "\n");
+
+        char charExample = 'a';
+        System.out.println("char: " + charExample + "\n");
+
+        
     }
 }

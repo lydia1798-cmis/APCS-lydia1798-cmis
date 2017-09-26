a41b902cd2d45b9af4a9ab2609f9410fd68a8119
Lydia Ritchie <lydiaritchie@Lydias-MacBook-Air.local>
2017-09-26 13:37:26 +0700

    working on Fundamentals 2

--- Fundamentals/Fundamentals2.java
+++ Fundamentals/Fundamentals2.java
@@ -10,12 +10,13 @@ public class Fundamentals2
 
         System.out.println("#2:");
         int[] arrayI = new int[]{2, 4, 6, 8, 3};
+        int[] arrayI2 = new int[]{1, 3, 5, 9};
         double[] arrayD = new double[]{2.5, 4.6, 6.7, 8.8, 3.9};
         boolean[] arrayB = new boolean[]{true, false, false, true, true};
         String[] arrayS = new String[]{"Tacos", "chips", "are", "really", "awesome"};
-        
-        boolean isSkipped = true;
-        
+
+        /*boolean isSkipped = true;
+
         printArray(arrayI, isSkipped);
         System.out.print("\n");
         printArray(arrayD, isSkipped);
@@ -24,24 +25,48 @@ public class Fundamentals2
         System.out.print("\n");
         printArray(arrayS, isSkipped);
         System.out.print("\n");
-        
+
         System.out.println("#3:");
         lastItem(arrayI);
         lastItem(arrayD);
         lastItem(arrayB);
         lastItem(arrayS);
         System.out.println("\n");
-        
+
         System.out.println("#4:");
         middleItem(arrayI);
         middleItem(arrayD);
         middleItem(arrayB);
         middleItem(arrayS);
         System.out.println("\n");
+
+        System.out.println("#5:");
+        randomInts(6, 2, 10);
+        System.out.println("\n");
+
+        System.out.println("#6:");
+        randomDoubles(6, 2.3, 10.8);
+        System.out.println("\n");
+
+        System.out.println("#7:");
+        copy(arrayI);
+        System.out.println("\n");
+
+        System.out.println("#8:");
+        pairs(9);
+        System.out.println("\n");
+
+        System.out.println("#9:");
+        concat(arrayI, arrayI2);
+        System.out.println("\n");*/
+
+        System.out.println("#10:");
+        merge(arrayI, arrayI2);
+        System.out.println("\n");
     }
 
     public static void printArray(int array[], boolean isSkipped){
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < array.length; i++){
             if(isSkipped == true && i % 2 == 0){
                 System.out.println(array[i]);
             }
@@ -52,7 +77,7 @@ public class Fundamentals2
     }
 
     public static void printArray(double array[], boolean isSkipped){
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < array.length; i++){
             if(isSkipped == true && i % 2 == 0){
                 System.out.println(array[i]);
             }
@@ -63,7 +88,7 @@ public class Fundamentals2
     }
 
     public static void printArray(boolean array[], boolean isSkipped){
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < array.length; i++){
             if(isSkipped == true && i % 2 == 0){
                 System.out.println(array[i]);
             }
@@ -72,9 +97,9 @@ public class Fundamentals2
             }
         }
     }
-    
+
     public static void printArray(String array[], boolean isSkipped){
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < array.length; i++){
             if(isSkipped == true && i % 2 == 0){
                 System.out.println(array[i]);
             }
@@ -85,48 +110,124 @@ public class Fundamentals2
     }
 
     public static int lastItem(int arrays[]){
-        System.out.println(arrays[4]);
-        return arrays[4]; 
+        int lastIndex = arrays.length - 1;
+        System.out.println(arrays[lastIndex]); 
+        return arrays[lastIndex];
     }
-    
+
     public static double lastItem(double arrays[]){
-        System.out.println(arrays[4]);
-        return arrays[4]; 
+        int lastIndex = arrays.length - 1;
+        System.out.println(arrays[lastIndex]); 
+        return arrays[lastIndex];
     }
-    
+
     public static boolean lastItem(boolean arrays[]){
-        System.out.println(arrays[4]);
-        return arrays[4]; 
+        int lastIndex = arrays.length - 1;
+        System.out.println(arrays[lastIndex]); 
+        return arrays[lastIndex];
     }
-    
+
     public static String lastItem(String arrays[]){
-        System.out.println(arrays[4]);
-        return arrays[4]; 
+        int lastIndex = arrays.length - 1;
+        System.out.println(arrays[lastIndex]); 
+        return arrays[lastIndex];
     }
-    
+
     public static int middleItem(int arrays[]){
-        System.out.println(arrays[2]);
-        return arrays[2]; 
-    }
-    
+        int middleIndex = arrays.length / 2;
+        System.out.println(arrays[middleIndex]);
+        return arrays[middleIndex];
+    } 
+
     public static double middleItem(double arrays[]){
-        System.out.println(arrays[2]);
-        return arrays[2]; 
-    }
-    
+        int middleIndex = arrays.length / 2;
+        System.out.println(arrays[middleIndex]);
+        return arrays[middleIndex];
+    } 
+
     public static boolean middleItem(boolean arrays[]){
-        System.out.println(arrays[2]);
-        return arrays[2]; 
-    }
-    
+        int middleIndex = arrays.length / 2;
+        System.out.println(arrays[middleIndex]);
+        return arrays[middleIndex];
+    } 
+
     public static String middleItem(String arrays[]){
-        System.out.println(arrays[2]);
-        return arrays[2]; 
-    }
-    
+        int middleIndex = arrays.length / 2;
+        System.out.println(arrays[middleIndex]);
+        return arrays[middleIndex];
+    } 
+
     public static int[] randomInts(int n, int min, int max){
         int[] array = new int[n];
-        math.random()
-        return array(n);
+        for(int i = 0; i < n; i++){
+            array[i] = min  + (int)(Math.random() * ((max - min) + 1));
+        } 
+        printArray(array, false);
+        return array;
     }
-}
+
+    public static double[] randomDoubles(int n, double min, double max){
+        double[] array = new double[n];
+        for(int i = 0; i < n; i++){
+            array[i] = min  + (double)(Math.random() * ((max - min) + 1));
+        } 
+        printArray(array, false);
+        return array;
+    }
+
+    public static int[] copy(int[] array){
+        int copiedArray [] = new int[array.length];
+        for(int i = 0; i < array.length; i++){
+            copiedArray[i] = array[i];
+        }
+        printArray(copiedArray, false);
+        return copiedArray;
+    }
+
+    public static int[] pairs(int n){
+        int[] pairs = new int[n * 2];
+        for(int i = 0, value = 2; i < pairs.length; i += 2, value += 2){
+            pairs[i] = value; 
+            pairs[i + 1] = value;
+        }
+        int[] myPairs = pairs;
+        printArray(myPairs, false);
+        return pairs;
+    }
+
+    public static int[] concat(int[] a, int[] b){
+        int combinedLength = a.length + b.length;
+        int[] combinedArray = new int[combinedLength];
+        for(int i = 0; i < a.length; i++){
+            combinedArray[i] = a[i];
+        }
+        for(int i = a.length, counter = 0; i < combinedArray.length; i++, counter++){
+            combinedArray[i] = b[counter];
+        }
+        printArray(combinedArray, false);
+        return combinedArray;
+    }
+
+    public static int[] merge(int[]a, int[]b){
+        int combinedLength = a.length + b.length;
+        int[] alternatingArray = new int[combinedLength];
+        int smallerArrayLength = 0;
+        if(a.length < b.length){
+            smallerArrayLength =+ a.length;
+        }
+        else{
+            smallerArrayLength =+ b.length;
+        }
+        int alternateLength = smallerArrayLength * 2;
+        for(int i = 0; i < combinedLength; i++){
+            if(alternatingArray.length >= alternateLength){
+                alternatingArray[i] = a[i];
+                alternatingArray[i + 1] = b[i];
+            }
+            else{
+            }
+            System.out.println("smallerArrayLength; " + smallerArrayLength + "\n");
+            printArray(alternatingArray, false);
+            return alternatingArray;
+        }
+    }

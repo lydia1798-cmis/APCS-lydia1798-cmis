c7c99677c0548a37d4b21af7bf51d3077a7b9441
cmis <cmis@cmis-hs07.(none)>
2017-10-05 11:34:00 +0700

    Yay i think i finished in time and i think that it might be right. it will really suck if it isnt right becuase i think it is

---
+++ FundamentalsIIQuiz1/ArrayMungerSol.java
@@ -0,0 +1,34 @@
+public class ArrayMungerSol
+{
+    public static void main(String[] args){
+        int[] a = {1, 2, 3};
+        int[] b = {1, 2, 3};
+        int[] r = munge(a, b);
+        for(int i: r){
+            System.out.println(i);
+        }
+    }
+    /**
+     * Write a method that takes 2 integer arrays as arguments.
+     * It should return the the array for which the sum of the first, last, and middle values is the greatest.
+     * If the sum is equal, it should return an array that contains the sum of the first values, 
+     * the sum of the middle values, and the sum of the last values.
+     */
+    public static int[] munge(int[] arr1, int[] arr2){
+        int sumArr1 = arr1[0] + arr1[arr1.length/2] + arr1[arr1.length-1];
+        int sumArr2 = arr2[0] + arr2[arr2.length/2] + arr2[arr2.length-1];
+        int sumFirst = arr1[0] + arr2[0];
+        int sumMiddle = arr1[arr1.length/2] + arr2[arr2.length/2];
+        int sumLast = arr1[arr1.length-1] + arr2[arr2.length-1];
+        if(sumArr1 == sumArr2){
+            int[] sumEqual = new int[]{sumFirst, sumMiddle, sumLast};
+            return sumEqual;
+        }
+        else if(sumArr1 > sumArr2){
+            return arr1;
+        }
+        else{
+            return arr2;
+        }
+    }
+}

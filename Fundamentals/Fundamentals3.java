0875c020e129c6d701e30c43744a6676a6f33672
cmis <cmis@cmis-hs07.(none)>
2017-10-05 11:53:28 +0700

    started fundamentals 3

---
+++ Fundamentals/Fundamentals3.java
@@ -0,0 +1,12 @@
+public class Fundamentals3{
+    //Main method
+    public static void main(String[] args){
+        int[][] defalutArray1 = new int[5][5];
+        int[][] literalArray1 = new int[][]{{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}};
+        
+    }
+    
+    
+    
+    
+}

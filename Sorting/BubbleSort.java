316b4548107b092363e82b0befa1626615e052ab
cmis <cmis@cmis-hs07.(none)>
2017-11-10 11:52:48 +0700

    can't figure anything out

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,46 @@
+
+/**
+ * Write a description of class BubbleSort here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BubbleSort
+{
+    public static void main(String[] args){
+        int[] numbers = new int[15];
+        for(int i = 0; i < numbers.length; i++){
+            numbers[i] = (int)(Math.random() * 100);
+        }
+        print(numbers);
+        sort(numbers);
+        print(numbers);
+    }
+
+    public static void print(int[] array){
+        for(int i : array){
+            System.out.println(i);
+        }
+        System.out.println();
+    }
+
+    public static void sort(int[] array){
+        boolean isSorted = false;
+        boolean switched = false;
+        while(!isSorted){
+            switched = false;
+            for(int i = 0; i < array.length - 1; i++){
+                int leftValue = array[i];
+                int rightValue = array[i + 1];
+                if(leftValue > rightValue){
+                    switched = true;
+                    array[i] = rightValue;
+                    array[i + 1] = leftValue;
+                }
+                if(switched == false){
+                    isSorted = true;
+                }
+            }
+        }
+    }
+}

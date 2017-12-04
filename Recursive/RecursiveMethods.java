692eeed00e3c74b597955f30af6a2cc2b0b8ee00
lydia1798-cmis <lydia1798@cmis.ac.th>
2017-12-04 11:53:52 +0700

    can't figure anything out

--- Recursive/RecursiveMethods.java
+++ Recursive/RecursiveMethods.java
@@ -2,8 +2,9 @@ public class RecursiveMethods
 {
     public static void main(String[] arg){
         //System.out.println(grid(2, 2, "@"));
-        //System.out.println(mul(2, 0));
-        System.out.println(binToDec("11111110"));
+        //System.out.println(power(2, 0));
+        //System.out.println(binToDec("11"));
+        //System.out.println(reverse("I can't do this"));
     }
 
     //Number 1:
@@ -36,13 +37,41 @@ public class RecursiveMethods
         }        
         return 0;
     }
-    
+    //POWER METHOD
+    public static int power(int n, int x){
+        //base case
+        if(x == 0){
+            return 1;
+        }
+        //recursive case
+        else{
+            return n * power(n, x - 1); //the value of the recursive call is what it returned last time
+        }        
+    }
     //Number 3:
-    public static String binToDec(String b){
-        //maximum length of binary number - 31 bits
-        //get the first character of the String 
-        String firstLetter = String.valueOf(b.charAt(0)); //THIS WORKS
-        return firstLetter;
-        //return b.length();
+    public static int binToDec(String b){
+        //maximum length of binary number - 31 bits        
+        //base case
+        if(b.length() == 0){
+            return 0;
+        }
+        else{
+            int integer = Integer.parseInt(b.substring(0,1));
+            return integer * power(2,b.length() - 1) + binToDec(b.substring(1, b.length()));                          
+        }
+    }
+    
+    public static String reverse(String s){
+       if(s.length() == 1){
+           return s;
+       }
+       else{
+           return s.substring(s.length() - 1) + reverse(s.substring(0, s.length() - 1));
+       }
+    }
+    
+    public static String remove(String s, String t){
+        //if(s.length()//idk       
+        return t;
     }
 }

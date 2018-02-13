f5d88405d45159ee09be76ca5d6e0288f7720c51
lyds <lyds>
2018-02-13 09:17:57 +0700

    asd

--- OOP/Song.java
+++ OOP/Song.java
@@ -4,15 +4,17 @@ public class Song
     private String name;
     private int bpm;
     private boolean isGood;
+    private int yearReleased;
 
     //zero argument constructor
     public Song(){
         this.name = "YMCA";
         this.bpm = 120;
         this.isGood = false;
+        this.yearReleased = 1980;
     }
 
-    public Song(String name, int bpm, boolean isGood){
+    public Song(String name, int bpm, boolean isGood, int yearReleased){
         if(name.equals ("YMCA")){
             this.name = "The Star Spangled Banner";
         }
@@ -26,13 +28,26 @@ public class Song
             this.bpm = bpm;
         }
         this.isGood = isGood;
+        this.yearReleased = yearReleased + 7;
+    }
+    
+    public int getYearReleased(){
+       return this.yearReleased; 
     }
 
+    public void setYearReleased(int yearReleased){
+        this.yearReleased = yearReleased;
+    }
+    
+    public String convertMinToSec(){
+    }
+    
     public String toString(){
         String out = "";
         out += "Name: " + name + "\n";
         out += "Beats per Minute: " + bpm + "\n";
         out += "This is a good song. " + isGood + "\n";
+        out += "The year this song was released was: " + yearReleased + "\n";
         return out;
     }
 }

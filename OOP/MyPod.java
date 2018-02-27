695faa6260326b9fcb984792479c17bff1315ebf
Lydia Ritchie <lydiaritchie@Lydias-MacBook-Air.local>
2018-02-27 18:09:22 +0700

    eh

--- OOP/MyPod.java
+++ OOP/MyPod.java
@@ -1,6 +1,16 @@
+import java.util.ArrayList;
 
 public class MyPod{    
     private String color;
     private int GB;
+    private ArrayList<String> songLibrary = new ArrayList<String>(3);
     
+    //Zero argument constructor
+    public MyPod(){
+        this.color = "blue";
+        this.GB = 12;
+        this.songLibrary.set(0, "YMCA");
+        this.songLibrary.set(1, "Here Comes The Sun");
+        this.songLibrary.set(2, "Yesterday");
+    }
 }
